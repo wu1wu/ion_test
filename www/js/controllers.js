6e75f4aad1870c5aabf1869f4156b43c3210adb8
Donovan Graham <donovan@platform7.com>
2014-06-30 23:14:50 +0200

    Fixing up tabs ui-srefs. Adding modal to delete

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp.controllers', []);
 app.controller('DashCtrl', ['$scope', function($scope) {
 }]);
 
-app.controller('TaksCtrl', ['$scope', 'TaskFactory', function($scope, Task) {
+app.controller('TaksCtrl', ['$scope', 'TaskFactory', '$ionicPopup', function($scope, Task, $ionicPopup) {
   $scope.tasks = Task.all();
 
   $scope.reorderTask = function(task, fromIndex, toIndex) {
@@ -12,10 +12,28 @@ app.controller('TaksCtrl', ['$scope', 'TaskFactory', function($scope, Task) {
   };
 
 
-  $scope.deleteTask = function(task) {
-    $scope.tasks.splice($scope.tasks.indexOf(task), 1);
-  };
+  // $scope.deleteTask = function(task) {
+  //   $scope.tasks.splice($scope.tasks.indexOf(task), 1);
+  // };
   
+
+   $scope.deleteTask = function(task) {
+     var _task = task;
+
+     var confirmPopup = $ionicPopup.confirm({
+       title: "something",
+       template: 'Are you sure you want to delete this task?'
+     });
+
+     confirmPopup.then(function(res) {
+       if(res) {
+         $scope.tasks.splice($scope.tasks.indexOf(_task), 1);
+       } 
+     });
+
+     return false;
+   };
+
 
 
 }]);
