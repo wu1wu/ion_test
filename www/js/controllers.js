1d5d76a87a6765899e39aa58fb3463bd07743ae5
Donovan Graham <donovan@platform7.com>
2014-06-30 23:37:25 +0200

    stopPropogation on delete click which prevents route transition. Prevent swipe to show buttons when in delete or re-order mode

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,37 +11,29 @@ app.controller('TaksCtrl', ['$scope', 'TaskFactory', '$ionicPopup', function($sc
     $scope.tasks.splice(toIndex, 0, task);
   };
 
+  $scope.deleteTask = function(task, $event) {
+    // $event.preventDefault();
+    $event.stopPropagation();
 
-  // $scope.deleteTask = function(task) {
-  //   $scope.tasks.splice($scope.tasks.indexOf(task), 1);
-  // };
-  
+    var _task = task;
 
-   $scope.deleteTask = function(task) {
-     var _task = task;
+    var confirmPopup = $ionicPopup.confirm({
+      title: _task.name,
+      template: 'Are you sure you want to delete this task?'
+    });
 
-     var confirmPopup = $ionicPopup.confirm({
-       title: "something",
-       template: 'Are you sure you want to delete this task?'
-     });
+    confirmPopup.then(function(res) {
+      if(res) {
+        $scope.tasks.splice($scope.tasks.indexOf(_task), 1);
+      } 
+    });
 
-     confirmPopup.then(function(res) {
-       if(res) {
-         $scope.tasks.splice($scope.tasks.indexOf(_task), 1);
-       } 
-     });
-
-     return false;
-   };
-
-
-
+    return false;
+  };
 }]);
 
 app.controller('TaskDetailCtrl', ['$scope', '$stateParams', 'TaskFactory', function($scope, $stateParams, Task) {
   $scope.task = Task.get($stateParams.taskId);
-
-
 }]);
 
 app.controller('AccountCtrl', ['$scope', function($scope) {
